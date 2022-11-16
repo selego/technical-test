daf78fb500168780f4f99cbae427b9d6bc5913e2
Mathias Bernardeau <mathias.bernardeau@maif.fr>
2022-11-16 10:55:12 +0100

    Fix crash while loading in project scene

--- app/src/scenes/project/view.js
+++ app/src/scenes/project/view.js
@@ -266,7 +266,7 @@ const Links = ({ project }) => {
           </a>
         </div>
       )}
-      {project.links.map((link) => (
+      {project?.links?.map((link) => (
         <div className="group text-sm font-medium	text-blue-700 border-[1px] border-blue-700 rounded-full overflow-hidden">
           <a target="blank" href={link.url} className="break-words cursor-pointer text-blue-700 hover:text-white hover:bg-blue-700 flex hover:no-underline h-full">
             <div className="flex items-center bg-blue-700 py-1 px-2 rounded-r-full ">
