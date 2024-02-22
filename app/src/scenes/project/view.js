1a53d12a6bfd783082af7ff9ffa94fccf908c420
Johannbr <johannbr@gmail.com>
2024-02-22 14:32:24 +0100

    fix : cannot open project view and cannot edit project and some boyscout rule :-)
    Replace ProjectObject.find by ProjectObject.findById as the endpoint "project/:id" must only return one project

--- app/src/scenes/project/view.js
+++ app/src/scenes/project/view.js
@@ -23,8 +23,8 @@ export default function ProjectView() {
 
   useEffect(() => {
     (async () => {
-      const { data: u } = await api.get(`/project/${id}`);
-      setProject(u);
+      const { data: project } = await api.get(`/project/${id}`);
+      setProject(project);
     })();
   }, []);
 
@@ -70,7 +70,7 @@ const ProjectDetails = ({ project }) => {
               <div className="flex justify-between gap-2">
                 <div className="flex gap-20">
                   <span className="w-fit text-[20px] text-[#0C1024] font-bold">Nom du projet : </span>
-                  <span className="w-fit text-[20px] text-[#0C1024] font-bold">{project.name.toString()}</span>
+                  <span className="w-fit text-[20px] text-[#0C1024] font-bold">{project.name?.toString()}</span>
                 </div>
                 <div className="flex flex-1 flex-column items-end gap-3">
                   <Links project={project} />
